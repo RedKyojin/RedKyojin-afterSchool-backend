f48f46b75063b5925d48e1d6dced01c168b8d0ce
Faizan <faizan579555@gmail.com>
2025-12-12 13:11:49 +0000

    Add POST/orders

--- server.js
+++ server.js
@@ -3,11 +3,15 @@ const express = require("express");
 const app = express();
 const PORT = 3000;
 
+// Middlewere
 // Logs requests
 app.use((req, res, next) => {
   console.log(`${req.method} ${req.url}`);
   next();
 });
+
+// Allow server to read JSON request bodies
+app.use(express.json());
 
 // Data
 const lessons = [
@@ -16,6 +20,8 @@ const lessons = [
   { id: 3, subject: "Chess", location: "R6", price: 25, space: 13 },
   { id: 4, subject: "Dodgeball", location: "GYM", price: 35, space: 3 },
 ];
+const orders = []; //Temporary array to hold JSON Post from postman.
+
 
 // Route
 app.get("/", (req, res) => {
@@ -26,6 +32,31 @@ app.get("/", (req, res) => {
 app.get("/lessons", (req, res) => {
   res.json(lessons);
 });
+
+
+// Save a new order
+app.post("/orders", (req, res) => {
+  const { name, phone, lessonIDs } = req.body;
+
+  if (!name || !phone || !lessonIDs || !Array.isArray(lessonIDs)) {
+    return res.status(400).json({ message: "Invalid order data" });
+  }
+
+  const newOrder = {
+    id: orders.length + 1,
+    name,
+    phone,
+    lessonIDs
+  };
+
+  orders.push(newOrder); //Sends Post to Array.
+
+  res.status(201).json({
+    message: "Order saved", //Express sends to postman.
+    order: newOrder
+  });
+});
+
 
 
 
