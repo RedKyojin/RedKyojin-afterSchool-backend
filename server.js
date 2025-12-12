0773d5751e248715a6c20a841fe092c98ccfa0ae
Faizan <faizan579555@gmail.com>
2025-12-12 11:59:59 +0000

    Add GET /lessons route

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+// Imports
 const express = require("express");
 const app = express();
 const PORT = 3000;
@@ -8,11 +9,27 @@ app.use((req, res, next) => {
   next();
 });
 
-// Test route
+// Data
+const lessons = [
+  { id: 1, subject: "Maths", location: "M5", price: 20, space: 9 },
+  { id: 2, subject: "Science", location: "S2", price: 20, space: 12 },
+  { id: 3, subject: "Chess", location: "R6", price: 25, space: 13 },
+  { id: 4, subject: "Dodgeball", location: "GYM", price: 35, space: 3 },
+];
+
+// Route
 app.get("/", (req, res) => {
   res.send("Hello");
 });
 
+// Returns lessons as JSON
+app.get("/lessons", (req, res) => {
+  res.json(lessons);
+});
+
+
+
+// Server
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
 });
